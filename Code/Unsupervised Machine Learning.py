d60d584d1e7eca7bddaaf97a74a62d7b4b4fdb37
wyattbaldwin <31597909+wyattbaldwin@users.noreply.github.com>
2023-01-04 22:28:18 -0500

    Update Unsupervised Machine Learning.py

--- Code/Unsupervised Machine Learning.py
+++ Code/Unsupervised Machine Learning.py
@@ -1,20 +1,37 @@
-from sklearn.cluster import KMeans
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, recall_score
 
 # Load the data
 df1 = pd.read_csv('pitch_arsenals.csv')
 df2 = pd.read_csv('poptime.csv')
 df3 = pd.read_csv('running_splits.csv')
 
-# Merge the dataframes
-df = pd.concat([df1, df2, df3], ignore_index=True)
+# Extract the features and labels
+X = data[['feature1', 'feature2', ...]]
+y = data['label']
 
-# Select the features to use for clustering
-X = df[['pitch_type', 'zone', 'count', 'pitch_number', 'release_speed', 'release_pos_x', 'release_pos_z', 'player_name', 'split_time', 'distance', 'acceleration', 'top_speed']]
+# Split the data into training and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-# Create and fit the model
-kmeans = KMeans(n_clusters=3)
-kmeans.fit(X)
+# Create a logistic regression model
+logistic_regression_model = LogisticRegression()
 
-# Predict the clusters for each data point
-predictions = kmeans.predict(X)
+# Train the model using the training data
+logistic_regression_model.fit(X_train, y_train)
+
+# Make predictions on the test data
+y_pred = logistic_regression_model.predict(X_test)
+
+# Evaluate the model's performance
+confusion_matrix = confusion_matrix(y_test, y_pred)
+accuracy = accuracy_score(y_test, y_pred)
+precision = precision_score(y_test, y_pred)
+recall = recall_score(y_test, y_pred)
+
+print('Confusion matrix:')
+print(confusion_matrix)
+print('Accuracy:', accuracy)
+print('Precision:', precision)
+print('Recall:', recall)
