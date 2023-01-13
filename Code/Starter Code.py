c0047064e8584c83bb85b1045178b32335cf9c4f
wyattbaldwin <31597909+wyattbaldwin@users.noreply.github.com>
2023-01-13 00:12:07 -0500

    Update Starter Code.py

--- Code/Starter Code.py
+++ Code/Starter Code.py
@@ -5,13 +5,11 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, recall_score
 
 # Load data from CSV files
-pitch_time = pd.read_csv("pitch_arsenals.csv")
-pop_time = pd.read_csv("poptime.csv")
-running_splits = pd.read_csv("running_splits.csv")
+running_splits = pd.read_csv("Database/running_splits_2022.csv")
 
 # Extract the features and labels
-X = data[['feature1', 'feature2', ...]]
-y = data['label']
+X = running_splits[['time_at_000ft', 'time_at_005ft', 'time_at_010ft', 'time_at_015ft','time_at_020ft','time_at_025ft','time_at_030ft','time_at_035ft','time_at_040ft','time_at_045ft','time_at_050ft','time_at_055ft','time_at_060ft','time_at_065ft','time_at_070ft','time_at_075ft','time_at_080ft','time_at_085ft','time_at_090ft']]
+y = running_splits['player_position_name']
 
 # Split the data into training and test sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
@@ -28,8 +26,8 @@ y_pred = logistic_regression_model.predict(X_test)
 # Evaluate the model's performance
 confusion_matrix = confusion_matrix(y_test, y_pred)
 accuracy = accuracy_score(y_test, y_pred)
-precision = precision_score(y_test, y_pred)
-recall = recall_score(y_test, y_pred)
+precision = precision_score(y_test, y_pred, average='weighted')
+recall = recall_score(y_test, y_pred, average='weighted')
 
 print('Confusion matrix:')
 print(confusion_matrix)
