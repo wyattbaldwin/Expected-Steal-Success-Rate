3ca045d0732968ae2fc8da98295ecd492479a0ff
wyattbaldwin <31597909+wyattbaldwin@users.noreply.github.com>
2023-01-13 16:37:34 -0500

    Update Running Splits.py

--- Code/Running Splits.py
+++ Code/Running Splits.py
@@ -1,36 +1,32 @@
-# Load necessary libraries
 import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, recall_score
 
-# Load data from CSV files
-running_splits = pd.read_csv("Database/running_splits_2022.csv")
+# Load data from spreadsheet
+data = pd.read_csv("Database/running_splits_2022.csv")
 
-# Extract the features and labels
-X = running_splits[['time_at_000ft', 'time_at_005ft', 'time_at_010ft', 'time_at_015ft','time_at_020ft','time_at_025ft','time_at_030ft','time_at_035ft','time_at_040ft','time_at_045ft','time_at_050ft','time_at_055ft','time_at_060ft','time_at_065ft','time_at_070ft','time_at_075ft','time_at_080ft','time_at_085ft','time_at_090ft']]
-y = running_splits['player_position_name']
+# Define the function to calculate the time at an unknown distance
+def interpolate_time(player_name, distance):
+    # Filter the data for the player
+    player_data = data[data["last_name"] == player_name]
+    
+    # Find the two known distances in feet that surround the unknown distance
+    distances = [col for col in player_data.columns if "time_at" in col]
+    distances = [float(d.replace("time_at_", "").replace("ft", "")) for d in distances]
+    distances.sort()
+    idx = min([i for i in range(len(distances)) if distances[i]>=distance])
+    lower_distance = distances[idx-1]
+    upper_distance = distances[idx]
 
-# Split the data into training and test sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-# Create a logistic regression model
-logistic_regression_model = LogisticRegression()
+    
+    # Use the corresponding known times to calculate the time at the unknown distance
+    lower_time = player_data[f"time_at_{str(lower_distance).zfill(3)}ft"].values[0]
+    upper_time = player_data[f"time_at_{str(upper_distance).zfill(3)}ft"].values[0]
+    interpolated_time = lower_time + (upper_time - lower_time) * (distance - lower_distance) / (upper_distance - lower_distance)
+    
+    return interpolated_time
 
-# Train the model using the training data
-logistic_regression_model.fit(X_train, y_train)
-
-# Make predictions on the test data
-y_pred = logistic_regression_model.predict(X_test)
-
-# Evaluate the model's performance
-confusion_matrix = confusion_matrix(y_test, y_pred)
-accuracy = accuracy_score(y_test, y_pred)
-precision = precision_score(y_test, y_pred, average='weighted')
-recall = recall_score(y_test, y_pred, average='weighted')
-
-print('Confusion matrix:')
-print(confusion_matrix)
-print('Accuracy:', accuracy)
-print('Precision:', precision)
-print('Recall:', recall)
+# Test the function
+player_name = "Abreu"
+distance = 27
+time = interpolate_time(player_name, distance)
+print(f"The estimated time for player {player_name} to reach {distance}ft is {time} seconds.")
