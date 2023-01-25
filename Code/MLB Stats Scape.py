447615a70e7eda4966d8909aa8a635f71e81112f
wyattbaldwin <31597909+wyattbaldwin@users.noreply.github.com>
2023-01-25 18:59:52 -0500

    Update MLB Stats Scape.py

--- Code/MLB Stats Scape.py
+++ Code/MLB Stats Scape.py
@@ -20,11 +20,14 @@ fixed_columns = ['PLAYER', 'YEAR', 'TEAM', 'G', 'AB', 'R', 'H', '2B', '3B', 'HR'
 with open(filename, 'w', newline='', encoding='utf-8') as csvfile:
     csv_writer = csv.writer(csvfile)
     for page_num in range(1, num_pages + 1):
-        time.sleep(random.uniform(1, 37)) # sleep random time between 1 and 37 sec
+        # sleep random time between 1 and 37 sec
+        time.sleep(random.uniform(1, 37))
+        
         # Construct the full URL
         url = base_url + '?page=' + str(page_num)
         response = requests.get(url)
         soup = BeautifulSoup(response.text, 'html.parser')
+        
         # Find the next page button
         next_button = soup.find('button', {'class': 'button-3wq5VxsJ paginationSide-3bpX4evu'})
         # if the next page button not found, stop looping
@@ -32,6 +35,7 @@ with open(filename, 'w', newline='', encoding='utf-8') as csvfile:
             break
         next_url = next_button.get('href')
         url = next_url
+        
         # Extract the data from the website
         table = soup.find('table')
         table_rows = table.find_all('tr')
