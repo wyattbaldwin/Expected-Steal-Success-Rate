7179fa8d669cc18e6764c51b22cdaf20c470d8a0
wyattbaldwin <31597909+wyattbaldwin@users.noreply.github.com>
2023-01-25 18:58:09 -0500

    Update MLB Stats Scape.py

--- Code/MLB Stats Scape.py
+++ Code/MLB Stats Scape.py
@@ -10,7 +10,7 @@ import time
 base_url = "https://www.mlb.com/stats/stolen-bases/all-time-by-season"
 
 # Number of pages to scrape
-num_pages = 50
+num_pages = 3798
 
 # Name of the CSV file to save the data to
 filename = "stolen_bases.csv"
@@ -19,7 +19,6 @@ fixed_columns = ['PLAYER', 'YEAR', 'TEAM', 'G', 'AB', 'R', 'H', '2B', '3B', 'HR'
 
 with open(filename, 'w', newline='', encoding='utf-8') as csvfile:
     csv_writer = csv.writer(csvfile)
-
     for page_num in range(1, num_pages + 1):
         time.sleep(random.uniform(1, 37)) # sleep random time between 1 and 37 sec
         # Construct the full URL
@@ -37,9 +36,21 @@ with open(filename, 'w', newline='', encoding='utf-8') as csvfile:
         table = soup.find('table')
         table_rows = table.find_all('tr')
         for tr in table_rows:
+            th_tag = tr.find('th', {'class': 'pinned-col-3lxtuFnc'})
+            if th_tag:
+                a_tag = th_tag.find('a', {'class': 'bui-link'})
+                if a_tag:
+                    player_name = " ".join(a_tag.stripped_strings)
+                else:
+                    player_name = ""
+            else:
+                player_name = ""
             if page_num == 1 and len(fixed_columns) > 0:  # this will help us only to extract the header of the table in the first page
                 csv_writer.writerow(fixed_columns)
                 fixed_columns = []
             td = tr.find_all('td')
             data = [i.get_text(strip=True) for i in td]
+            data.insert(0, player_name)
             csv_writer.writerow(data)
+        print("Scraped page", page_num)
+    print("Scraping complete!")
